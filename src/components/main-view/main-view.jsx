778233196d6a68308d57f0399f45d77211861fc6
hand1ov1eternity <absurdism@duck.com>
2024-12-09 13:03:33 +0100

    adding my api movie data and use proptypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,33 +2,26 @@ import { useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
-export const MainView = ({movie, onBackCLick}) => {
-  const [movies, setMovies] = useState([
-    { id: 1, 
-      title: "Inception",
-      description: "A thrilling adventure through the mind, exploring the boundaries of dreams and reality.",
-      genre: "Sci-Fi",
-      director: "Christopher Nolan",
-      imageURL: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg"
-    },
-   
-      { id: 2, 
-        title: "The Dark Knight",
-        description: "A gritty portrayal of Batman facing the Joker in Gotham.",
-        genre: "Action",
-        director: "Christopher Nolan",
-        imageURL: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg"
-    },
-    
-      { id: 3, 
-        title: "The Shining",
-        description: "A writer descends into madness while isolated in a haunted hotel.",
-        genre: "Horror",
-        director: "Stanley Kubrick",
-        imageURL: "https://m.media-amazon.com/images/M/MV5BMTg0MzkzODUwNV5BMl5BanBnXkFtZTgwODM1MjEwNDI@._V1_.jpg"
+export const MainView = ({movies, onBackCLick}) => {
+  const [movies, setMovies] = useState([]);
 
-      }
-  ]);
+  useEffect(() => {
+    fetch("/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            description: doc.description,
+            genre: doc.genre,
+            director: doc.director_name?.[0]
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
